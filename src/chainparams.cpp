ab255135be319468f6cbb9ecf0295b67bce2cf52
xziy - xhozt <cziy@ya.ru>
2017-11-17 02:14:07 +0400

    test

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -72,7 +72,7 @@ public:
         std::vector<CTxOut> vout;
         vout.resize(1);
         vout[0].SetEmpty();
-        CTransaction txNew(1, 1510871400, vin, vout, 100);
+        CTransaction txNew(1, 1510871400, vin, vout, 1);
         genesis.vtx.push_back(txNew);
         genesis.hashPrevBlock = 0;
         genesis.hashMerkleRoot = genesis.BuildMerkleTree();
